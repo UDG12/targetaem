19f7d8a0c820e15da7dedbdcd99ff6407dffc1c0
Udit <udit1996gupta@gmail.com>
2024-05-08 18:25:09 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -92,7 +92,7 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
  */
 export default async function decorate(block) {
   // load nav as fragment
-  const navMeta = getMetadata('target');
+  const navMeta = getMetadata('nav');
   const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
   const fragment = await loadFragment(navPath);
 
