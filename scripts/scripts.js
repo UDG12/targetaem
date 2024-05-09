212e3ff6db59932d014f4491f688fa4d2c440474
Udit <udit1996gupta@gmail.com>
2024-05-09 12:32:39 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -137,6 +137,7 @@ let alloyLoadedPromise = initWebSDK('./alloy.js', {
     datastreamId: 'ce2eeece-8320-4043-b94a-4c13f308248d',
     orgId: '73D97EE25CCCE8260A495EBD@AdobeOrg',
   });
+alloyLoadedPromise.then(() => getAndApplyRenderDecisions());
 //if (getMetadata('nav')) {
   //alloyLoadedPromise.then(() => getAndApplyRenderDecisions());
 //}
